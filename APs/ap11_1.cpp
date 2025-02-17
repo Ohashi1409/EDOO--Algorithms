3ca4cb1c70dc039638cd90805241278ef3415f33
João Ohashi <131549440+Ohashi1409@users.noreply.github.com>
2025-02-17 13:41:56 -0300

    Open Hashing concluded and ap11 done

--- APs/ap11_1.cpp
+++ APs/ap11_1.cpp
@@ -1,30 +1,58 @@
 #include <iostream>
+#include <string>
+#include <list>
 
 using namespace std;
 
-template <typename List>
 class Dict {
 private:
     int m;
-    int cnt;
-    List* H;
+    list<int> *H;
 public:
     Dict(int size) {
         this->m = size;
-        this->cnt = 0;
-        this->H = new List[size];
-        for (int i = 0; i < size; i++) {
-            this->H[i] = new List[];
+        this->H = new list<int>[size];
+    }
+
+    int h(int element) { return (element%10); }
+
+    bool find(int element) {
+        int pos = this->h(element);
+        for (int num : this->H[pos]) {
+           if (num == element) { return true; }
+        }
+        return false;
+    }
+
+    void insert(int element) {
+        if (!find(element)) {
+            int pos = this->h(element);
+            this->H[pos].push_back(element);
         }
     }
 
-    void insert() {
-        
+    void print() {
+        for (int i = 0; i < this->m; i++) {
+            cout << i << " ";
+            for (int value : this->H[i]) {
+                cout << value << " ";
+            }
+            cout << endl;
+        }
     }
 };
 
 int main() {
-    
+    int qtd_num;
+    cin >> qtd_num;
+    Dict dict(10);
+    for (int i = 0; i < qtd_num; i++) {
+        int num;
+        cin >> num;
+        dict.insert(num);
+    }
+
+    dict.print();
 
     return 0;
 }
